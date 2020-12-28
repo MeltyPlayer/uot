8ff5ba37403f6cf4e7bbb9d079a2c252b09aedcb
MeltyPlayer <mastergamepro@hotmail.com>
2020-12-27 23:52:14 -0600

    Added a new file that will be used in the future to set which tunic color link is wearing.

---
+++ Utility of Time CSharp/actor/link/Tunic.cs
@@ -0,0 +1,20 @@
+﻿namespace UoT {
+  public class Tunic {
+    /**
+     * Selected tunic id @ 15E6D0
+     * - 17: kokiri
+     * - 18: goron
+     * - 19: zora
+     *
+     * Doesn't impact color until unpause.
+     */
+
+    public const int TUNIC_TYPE_RDRAM_OFFSET = 0x2246FC;
+
+    public enum TunicType {
+      KOKIRI = 0,
+      GORON = 1,
+      ZORA = 2,
+    }
+  }
+}
