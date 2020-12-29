c82d5fdaed15067a87341c806f3ebe399545f89f
MeltyPlayer <mastergamepro@hotmail.com>
2020-12-28 23:35:24 -0600

    Added a new class for setting the environment color on specific models. It doesn't actually do anything yet... this will need to get plugged into logic in the future.

---
+++ Utility of Time CSharp/hacks/EnvironmentColorHacks.cs
@@ -0,0 +1,16 @@
+﻿namespace UoT {
+  /// <summary>
+  ///   Hack class for setting specific environment colors for given models.
+  /// </summary>
+  public class EnvironmentColorHacks {
+    // TODO: Add a tab for these toggles.
+    // TODO: Add toggle for Link's tunic.
+
+    /**
+     *     ShaderManager.SetEnvironmentColor(Tunic.TUNIC_COLOR_KOKIRI(0) / 255.0F,
+                                      Tunic.TUNIC_COLOR_KOKIRI(1) / 255.0F,
+                                      Tunic.TUNIC_COLOR_KOKIRI(2) / 255.0F,
+                                      Tunic.TUNIC_COLOR_KOKIRI(3) / 255.0F)
+     */
+  }
+}
