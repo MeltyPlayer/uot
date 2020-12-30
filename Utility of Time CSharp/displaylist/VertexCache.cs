dc2a43df5cea320d3903d3ff790ae4c7cc82ddd4
MeltyPlayer <mastergamepro@hotmail.com>
2020-12-30 15:46:18 -0600

    Set up a new helper class for caching vertices.

---
+++ Utility of Time CSharp/displaylist/VertexCache.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UoT {
+  public struct Vertex {
+    public bool Populated;
+
+    public short X;
+    public short Y;
+    public short Z;
+
+    public short U;
+    public short V;
+
+    public byte R;
+    public byte G;
+    public byte B;
+    public byte A;
+  } 
+
+  public class VertexCache {
+    private readonly Vertex[] impl_;
+
+    public VertexCache() {
+      this.impl_ = new Vertex[32];
+    }
+
+    public void Reset() {
+      for (var i = 0; i < this.impl_.Length; ++i) {
+        this.impl_[i] = default;
+      }
+    }
+
+    public Vertex this[int index] {
+      get => this.impl_[index];
+      set => this.impl_[index] = value;
+    }
+  }
+}
