275c086cfe2ac2eb5a71d346432c9a70a69eef3e
MeltyPlayer <mastergamepro@hotmail.com>
2020-12-30 23:41:09 -0600

    Set up a new blank helper class for encapsulating animation playback logic.

---
+++ Utility of Time CSharp/ui/main/tabs/animation/AnimationPlaybackManager.cs
@@ -0,0 +1,22 @@
+﻿namespace UoT {
+  /// <summary>
+  ///   Helper interface for managing the dirty details of playing back an
+  ///   animation for a model.
+  /// </summary>
+  public interface IAnimationPlaybackManager {
+
+    double CurrentFrame { get; }
+    int TotalFrames { get; }
+    int FrameRate { get; }
+
+    bool IsPlaying { get; }
+    bool ShouldLoop { get; }
+    void Play();
+    void Pause();
+  }
+
+  // TODO: Move all animation logic and data from UI components into this
+  // class.
+  public class AnimationPlaybackManager {
+  }
+}
