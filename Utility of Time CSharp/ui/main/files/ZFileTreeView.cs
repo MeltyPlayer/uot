03e76359db4bbc761845ebeb66b371260fd43bc1
MeltyPlayer <mastergamepro@hotmail.com>
2021-01-03 18:49:08 -0600

    Cleaned up the file tree logic by creating a generic wrapper for the common tree view component.

--- Utility of Time CSharp/ui/main/files/ZFileTreeView.cs
+++ Utility of Time CSharp/ui/main/files/ZFileTreeView.cs
@@ -4,6 +4,7 @@ using System.Windows.Forms;
 
 using UoT.common.fuzzy;
 using UoT.memory.files;
+using UoT.ui.common.component;
 
 namespace UoT.ui.main.files {
   public partial class ZFileTreeView : UserControl {
@@ -13,9 +14,9 @@ namespace UoT.ui.main.files {
     // TODO: Add support for different hierarchies.
     // TODO: Clean up the logic here.
 
-    private ZFiles data_;
+    private readonly BetterTreeView<ZFileNode> betterTreeView_;
+
     private LinkedList<ZFileNode> zFileNodes_ = new LinkedList<ZFileNode>();
-    private readonly FuzzyTreeComparer comparer_;
 
     private readonly IFuzzySearchDictionary<ZFileNode> filterImpl_ =
         new SymSpellFuzzySearchDictionary<ZFileNode>();
@@ -26,9 +27,7 @@ namespace UoT.ui.main.files {
 
     // TODO: Clean this up.
     private class ZFileNode {
-      public string Text;
       public ZFileNode Parent;
-      public TreeNode TreeNode;
       public IZFile ZFile;
       public ISet<string> Keywords;
       public double MatchPercentage;
@@ -40,72 +39,69 @@ namespace UoT.ui.main.files {
 
       this.filterTextBox_.TextChanged += (sender, args) => { this.Filter_(); };
 
-      this.fileTreeView_.AfterSelect += (sender, args) => {
-        var selectedFile = this.GetSelectedFile_();
+      this.betterTreeView_ = new BetterTreeView<ZFileNode>(this.fileTreeView_);
 
+      this.betterTreeView_.Selected += betterTreeNode => {
+        var selectedFile = betterTreeNode?.AssociatedData?.ZFile;
         if (selectedFile != null) {
           this.FileSelected.Invoke(selectedFile);
         }
       };
-
-      this.fileTreeView_.TreeViewNodeSorter =
-          this.comparer_ = new FuzzyTreeComparer();
     }
 
     public void Populate(ZFiles zFiles) {
-      this.fileTreeView_.BeginUpdate();
-      this.comparer_.Enabled = false;
+      this.betterTreeView_.BeginUpdate();
 
-      var nodes = this.fileTreeView_.Nodes;
+      var root = this.betterTreeView_.Root;
 
-      var modelsNode = nodes.Add("Actor models");
+      var modelsNode = root.Add("Actor models");
       this.AddZFileNodeFor(null, modelsNode);
       foreach (var model in zFiles.Objects) {
-        var modelNode = modelsNode.Nodes.Add(model.BetterFileName);
+        var modelNode = modelsNode.Add(model.BetterFileName);
         this.AddZFileNodeFor(model, modelNode);
       }
 
-      var actorCodeNode = nodes.Add("Actor code");
+      var actorCodeNode = root.Add("Actor code");
       this.AddZFileNodeFor(null, actorCodeNode);
       foreach (var code in zFiles.ActorCode) {
-        var codeNode = actorCodeNode.Nodes.Add(code.BetterFileName);
+        var codeNode = actorCodeNode.Add(code.BetterFileName);
         this.AddZFileNodeFor(code, codeNode);
       }
 
-      var scenesNode = nodes.Add("Scenes");
+      var scenesNode = root.Add("Scenes");
       this.AddZFileNodeFor(null, scenesNode);
       foreach (var scene in zFiles.Scenes) {
-        var sceneNode = scenesNode.Nodes.Add(scene.BetterFileName);
+        var sceneNode = scenesNode.Add(scene.BetterFileName);
         this.AddZFileNodeFor(scene, sceneNode);
 
         foreach (var map in scene.Maps) {
-          var mapNode = sceneNode.Nodes.Add(map.BetterFileName);
+          var mapNode = sceneNode.Add(map.BetterFileName);
           this.AddZFileNodeFor(map, mapNode);
         }
       }
 
-      var othersNode = nodes.Add("Others");
+      var othersNode = root.Add("Others");
       this.AddZFileNodeFor(null, othersNode);
       foreach (var other in zFiles.Others) {
-        var otherNode = othersNode.Nodes.Add(other.BetterFileName);
+        var otherNode = othersNode.Add(other.BetterFileName);
         this.AddZFileNodeFor(other, otherNode);
       }
 
       this.InitializeAutocomplete_();
 
-      this.fileTreeView_.EndUpdate();
-      this.comparer_.Enabled = true;
+      this.betterTreeView_.EndUpdate();
     }
 
-    private void AddZFileNodeFor(IZFile zFile, TreeNode treeNode) {
-      var parentZFileNode = (ZFileNode) treeNode.Parent?.Tag;
+    private void AddZFileNodeFor(
+        IZFile zFile,
+        BetterTreeNode<ZFileNode> treeNode) {
+      var parentZFileNode = treeNode.Parent?.AssociatedData;
 
       var zFileNode = new ZFileNode {
           Parent = parentZFileNode,
-          Text = treeNode.Text,
-          TreeNode = treeNode,
           ZFile = zFile,
       };
+      treeNode.AssociatedData = zFileNode;
 
       this.zFileNodes_.AddLast(zFileNode);
       parentZFileNode?.AllChildZFileNodes?.Add(zFileNode);
@@ -126,10 +122,6 @@ namespace UoT.ui.main.files {
           this.filterImpl_.Add(keyword, zFileNode);
         }
       }
-
-      // TODO: Is there a better option that doesn't use boxing?
-      // Adds as tag.
-      treeNode.Tag = zFileNode;
     }
 
     private void InitializeAutocomplete_() {
@@ -144,54 +136,40 @@ namespace UoT.ui.main.files {
       this.filterTextBox_.AutoCompleteCustomSource = allAutocompleteKeywords;
     }
 
-    private IZFile GetSelectedFile_() {
-      var zFileNode = (ZFileNode) this.fileTreeView_.SelectedNode?.Tag;
-      return zFileNode?.ZFile;
-    }
-
     // TODO: Debounce this method.
     private void Filter_() {
       var filterText = this.filterTextBox_.Text.ToLower();
 
       this.ResetMatchPercentages_();
 
-      this.fileTreeView_.BeginUpdate();
-      this.comparer_.Enabled = false;
-
-      this.ClearTree_();
+      this.betterTreeView_.BeginUpdate();
 
       if (string.IsNullOrEmpty(filterText)) {
-        this.fileTreeView_.TreeViewNodeSorter = null;
-        this.HideMatchesLessThan_(0);
+        this.betterTreeView_.Root.ResetChildrenRecursively();
+
+        this.betterTreeView_.Comparer = null;
       } else {
         const float matchPercentage = 20;
 
         var matches = this.filterImpl_.Search(filterText, matchPercentage);
 
         this.PropagateMatchPercentages_(matches);
-        this.HideMatchesLessThan_(matchPercentage);
+        this.betterTreeView_.Root.ResetChildrenRecursively(
+            betterTreeNode
+                => betterTreeNode.AssociatedData.MatchPercentage >=
+                   matchPercentage);
+
+        this.betterTreeView_.Comparer =
+            this.betterTreeView_.Comparer ?? new FuzzyTreeComparer();
       }
 
-      this.fileTreeView_.EndUpdate();
-      this.comparer_.Enabled = true;
+      this.betterTreeView_.EndUpdate();
     }
 
     private void ResetMatchPercentages_() {
       foreach (var zFileNode in this.zFileNodes_) {
         zFileNode.MatchPercentage = 0;
       }
-    }
-
-    private void ClearTree_(TreeNode root = null) {
-      var treeNodes = (root == null)
-                          ? this.fileTreeView_.Nodes
-                          : root.Nodes;
-
-      foreach (var treeNode in treeNodes) {
-        this.ClearTree_((TreeNode) treeNode);
-      }
-
-      treeNodes.Clear();
     }
 
     private void PropagateMatchPercentages_(
@@ -218,60 +196,12 @@ namespace UoT.ui.main.files {
       ZFileTreeView.SetMatchPercentage_(parentZFileNode, matchPercentage);
     }
 
-    private void HideMatchesLessThan_(
-        double minMatchPercentage,
-        ZFileNode root = null) {
-      // TODO: Merge these for loops.
-      if (root == null) {
-        foreach (var zFileNode in this.zFileNodes_) {
-          if (zFileNode.Parent != null) {
-            continue;
-          }
-
-          if (zFileNode.MatchPercentage < minMatchPercentage) {
-            continue;
-          }
-
-          var treeNode = zFileNode.TreeNode;
-          treeNode.Text = minMatchPercentage < .001f
-                              ? zFileNode.Text
-                              : $"{zFileNode.Text} ({zFileNode.MatchPercentage:0.0}%)";
-          this.fileTreeView_.Nodes.Add(treeNode);
-
-          this.HideMatchesLessThan_(minMatchPercentage, zFileNode);
-        }
-      } else {
-        foreach (var zFileNode in root.AllChildZFileNodes) {
-          if (zFileNode.MatchPercentage < minMatchPercentage) {
-            continue;
-          }
-
-          var treeNode = zFileNode.TreeNode;
-          treeNode.Text = minMatchPercentage < .001f
-                              ? zFileNode.Text
-                              : $"{zFileNode.Text} ({zFileNode.MatchPercentage:0.0}%)";
-          root.TreeNode.Nodes.Add(treeNode);
-
-          this.HideMatchesLessThan_(minMatchPercentage, zFileNode);
-        }
-      }
-    }
-
-    private class FuzzyTreeComparer : IComparer {
-      public bool Enabled { get; set; }
-
-      public int Compare(object lhs, object rhs) {
-        if (!this.Enabled) {
-          return 0;
-        }
-
-        // TODO: Can we eliminate unboxing (casting from object)?
-        var lhsZFileNode = (ZFileNode) (((TreeNode) lhs).Tag);
-        var rhsZFileNode = (ZFileNode) (((TreeNode) rhs).Tag);
-
-        return -lhsZFileNode.MatchPercentage.CompareTo(
-                   rhsZFileNode.MatchPercentage);
-      }
+    private class FuzzyTreeComparer : IComparer<BetterTreeNode<ZFileNode>> {
+      public int Compare(
+          BetterTreeNode<ZFileNode> lhs,
+          BetterTreeNode<ZFileNode> rhs)
+        => -lhs.AssociatedData.MatchPercentage.CompareTo(
+               rhs.AssociatedData.MatchPercentage);
     }
   }
 }
