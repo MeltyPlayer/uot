e1181596e4a65777b9a29730069dd61a7a4b30dd
MeltyPlayer <mastergamepro@hotmail.com>
2021-01-10 02:29:25 -0600

    Added support for blue tektites.

--- Utility of Time CSharp/hacks/face/TektiteIndirectTextureHack.cs
+++ Utility of Time CSharp/hacks/face/TektiteIndirectTextureHack.cs
@@ -8,22 +8,28 @@
   /// </summary>
   public class TektiteIndirectTextureHack : IIndirectTextureHack {
     // TODO: Support switching between the sets in the editor.
-    // TODO: Where is blue?
 
     public uint MapTextureAddress(uint originalAddress) {
+      var blueOffset = 0x1300;
+      var redOffset = 0x1B00;
+
+      var topOffset = blueOffset;
+      var eyeOffset = topOffset + (0x1F20 - 0x1B00);
+      var bottomOffset = topOffset + (0x2100 - 0x1B00);
+
       // Top
       if (originalAddress == 0x08000000) {
-        return 0x06000000 + 0x1B00;
+        return 0x06000000 + (uint) topOffset;
       }
 
       // Eye
       if (originalAddress == 0x09000000) {
-        return 0x06000000 + 0x1F20;
+        return 0x06000000 + (uint) eyeOffset;
       }
 
       // Bottom
       if (originalAddress == 0x0A000000) {
-        return 0x06000000 + 0x2100;
+        return 0x06000000 + (uint) bottomOffset;
       }
 
       return originalAddress;
